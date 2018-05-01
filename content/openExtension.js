dc12f5ed4199169e6188ab659571ff1c891b6464
Joseph Ellyson <joe91e@gmail.com>
2018-04-30 22:20:05 -0500

    remove the button injection.

--- content/openExtension.js
+++ content/openExtension.js
@@ -1,11 +1,5 @@
-var openDynamicPostsButton = "<button id='dynamicPostsButton'>My Top40 List</button>";
-$('body').append(openDynamicPostsButton);
-$('#dynamicPostsButton').click(function(){
-  chrome.runtime.sendMessage({
-    type: 'openDynamicPosts',
-    replaceTab: true,
-    params: window.location.search
-  });
+chrome.runtime.sendMessage({
+  type: 'openDynamicPosts',
+  replaceTab: true,
+  params: window.location.search
 });
-
-$('#dynamicPostsButton').click();
